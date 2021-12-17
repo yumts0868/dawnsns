d1924a8745f1185ab42cff9c1bafab3558985b56
yumts <yu.falcon.0860.to@gmail.com>
2021-12-17 19:28:21 +0900

    検索機能の追加

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -2,11 +2,12 @@
 
 @section('content')
 
-<form action="/create" method="post">
+<form class="wrap-post" action="/create" method="post">
+  <img class="wrap-post-icon" src="images/dawn.png">
   {{ csrf_field() }}
-  <input type="text" name="post">
+  <input class="wrap-post-form" type="text" name="post" placeholder="何をつぶやこうか…？">
   <input type="hidden" name="userid">
-  <input type="image" src="images/post.png">
+  <input class="wrap-post-form-btn" type="image" src="{{asset('images/post.png')}}">
 
 </form>
 
@@ -14,10 +15,33 @@
   <tr>
     <td><img src="{{asset('images/dawn.png')}}"></td>
     <td>{{$list->username}}</td>
-    <td>{{$list->posts}}</td>
+    <td>{{$list->post}}</td>
     <td>{{$list->created_at}}</td>
+          <!-- モーダル編集ボタン -->
     <td>
-
+       <div class="edit_modal">
+         <a href="" class="modalopen"  data-target="modal{{$list->id}}">
+           <input type="image" src="{{asset('images/edit.png')}}">
+         </a>
+       </div>
+     </td>
+           <!-- モーダル削除ボタン -->
+     <td class="delete-modal">
+       <form  method="post" action="/delete/{{ $list->id }}">
+       <input type= "image" id="pic" src="{{asset('images/trash.png')}}" onClick="return Check()" onmouseover="hoge();" onmouseout="foo();" >
+     </td>
+</tr>
+<!-- モーダルウィンドウ -->
+<div class="modal-inner" id="modal{{$list->id}}">
+   <div class="inner-content">
+      <p class="inner-title">編集画面</p>
+      <form method="post" action="/edit/{{$list->id}}">
+      {{csrf_field()}}
+      <input type="text" name="edit" value="{{$list->post}}">
+      <input type= "image" src="{{asset('images/edit.png')}}">
+      </form>
+   </div>
+ </div>
 
 @endforeach
 
--- resources/views/users/search.blade.php
+++ resources/views/users/search.blade.php
@@ -2,6 +2,45 @@
 
 @section('content')
 
+<form action="/result" method="post">
+{{ csrf_field() }}
+<input type="text" name="search">
+<input type= "image" class="search-p" src="{{ asset('images/search.png') }}">
+</form>
+
+@foreach($list as $list)
+<tr>
+     <div class="wrap-search">
+          <div class="wrap-search-form">
+            <td><img class="icon" src="images/{{$list->images}}"></td>
+            <td><?php echo $list->username ; ?></td>
+           </div>
+
+      @if(!in_array($list->id,array_column($followUser,'follow')))
+      <td>
+          <div class="wrap-userlist-item">
+           <form action="/follow" method="post">
+           {{ csrf_field() }}
+           <button type="submit" value="{{$list->id}}" name="follow">フォローする</button>
+           </form>
+          </div>
+      </td>
+     </div>
+     @else
+     <td>
+          <div class="wrap-userlist-item">
+           <form action="/unfollow" method="post">
+           {{ csrf_field() }}
+           <button type="submit" value="{{$list->id}}" name="unfollow">フォローをはずす</button>
+           </form>
+          </div>
+     </td>
+     @endif
+
+</tr>
+
+@endforeach
+
 
 
 @endsection
